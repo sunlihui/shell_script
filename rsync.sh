1e0f03a2e2244e082809e6b7cf4e54301612aaff
Sunlihui <365793275@qq.com>
2017-12-12 16:14:21 +0800

    del passwd

--- rsync.sh
+++ rsync.sh
@@ -13,20 +13,20 @@ PATH2=/*/webapps
 for i in `ls $PATH1`
 do 
 cd $PATH1
-rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords --exclude=logs ${i} qk365@10.10.10.200::tomcats_10.10.10.152;
+rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords --exclude=logs ${i} user@ip::tomcats_ip;
 done
 
 #2 Backup webapps
 for i in `ls $PATH2`
 do
 cd $PATH2
-rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords --exclude=WEB-INF/logs ${i} qk365@10.10.10.200::webapps_10.10.10.152;
+rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords --exclude=WEB-INF/logs ${i} user@ip::webapps_ip;
 done
 
 #3 Backup appsconfig 
-rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords  /qingke/appsconfig  qk365@10.10.10.200::appsconfig_10.10.10.152
+rsync -vzrtopg  --progress --delete --password-file=/etc/rsyncd.passwords  /qingke/appsconfig  user@ip::appsconfig_ip
 
 #4 Backup nginx log
-#rsync -vzrtopg --delete   --password-file=/etc/rsyncd.passwords  /etc/nginx/  qk365@10.10.10.200::nginx_10.10.10.151
+#rsync -vzrtopg --delete   --password-file=/etc/rsyncd.passwords  /etc/nginx/  user@ip::nginx_ip
 
 
